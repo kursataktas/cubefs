c7f180ac349c2e11cc48a2a39de3abd34a36cb62
true1064 <tangjingyu@oppo.com>
2024-01-10 20:16:08 +0800

    feat(meta): [hybrid cloud] handle compatibility when old version client send createInode request without field StorageClass #21960067 Signed-off-by: true1064 <tangjingyu@oppo.com>

--- metanode/const.go
+++ metanode/const.go
@@ -245,7 +245,7 @@ const (
 	metaNodeDeleteBatchCountKey = "batchCount"
 	configNameResolveInterval   = "nameResolveInterval" // int
 
-	cfgDefaultMediaType = "defaultMediaType"
+	cfgLegacyStorageClass = "legacyReplicaStorageClass"
 )
 
 const (
